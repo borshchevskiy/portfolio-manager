cde4f5d926383cd3e2666e497065c527e149a9de
Ilya <freer1der.msf@gmail.com>
2024-09-20 20:02:11 +0300

    Added liquibase and user table migrations

--- portfolio-service/src/main/java/ru/borshchevskiy/portfolioservice/model/User.java
+++ portfolio-service/src/main/java/ru/borshchevskiy/portfolioservice/model/User.java
@@ -1,7 +1,9 @@
 package ru.borshchevskiy.portfolioservice.model;
 
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
@@ -20,9 +22,13 @@ public class User {
     private String lastname;
     @Column("email")
     private String email;
+    @Column("password")
+    private String password;
     @Column("created_date")
+    @CreatedDate
     private LocalDateTime createdDate;
     @Column("last_modified_date")
+    @LastModifiedDate
     private LocalDateTime lastModifiedDate;
 }
 
